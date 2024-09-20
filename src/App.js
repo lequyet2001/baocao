e63845ebdfacadb5560b78bdaf7e356850fe7f19
quyet <quyet.lh198327@sis.hust.edu.vn>
2024-09-20 14:17:56 +0700

    q

--- src/App.js
+++ src/App.js
@@ -63,14 +63,13 @@ function App() {
           Tạo báo cáo
         </button>
         <div className="bao-cao-output mt-6">
-          <TextSnippetCopier value={`${name && `${name.split('-')[0] || ''} - Phòng 7 - Báo cáo công việc ${name.split('-')[1] || ''} ${now}.`}
-  ${baoCao.done.length > 0 ? `Công việc đã làm:\n${baoCao.done.map(e => `- ${e}`).join('\n')}` : ''}
-  ${baoCao.upcoming.length > 0 ? `Dự kiến:\n${baoCao.upcoming.map(e => `- ${e}`).join('\n')}` : ''}`}/>
+          <TextSnippetCopier value={`${name && `${name.split('-')[0] || ''} - Phòng 7 - Báo cáo công việc ${name.split('-')[1] || ''} ${now}.`}${'\n'}${baoCao.done.length > 0 ? `Công việc đã làm:\n${baoCao.done.map(e => `- ${e}`).join('\n')}` : ''}${'\n'}${baoCao.upcoming.length > 0 ? `Dự kiến:\n${baoCao.upcoming.map(e => `- ${e}`).join('\n')}` : ''}`}/>
         </div>
       </div>
     </div>
   );
 }
+
 
 export default App;
 
