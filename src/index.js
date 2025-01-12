e4b4591b86387c8497a9addb5dd9220a0af3c802
quyet <quyet.lh198327@sis.hust.edu.vn>
2025-01-12 11:36:25 +0700

    Refactor router to use createHashRouter and clean up code structure

--- src/index.js
+++ src/index.js
@@ -2,27 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter,RouterProvider} from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
-const router =createBrowserRouter([
+const router = createHashRouter([
   {
-    path:'/baocao',
-    element:<App/>,
-    // errorElement:<div>Not found</div>
+    path: '/baocao',
+    element: <App />,
   },
   {
-    path:'/baocao/:id',
-    element:<div>Detail</div>,
-  }
-])
+    path: '/baocao/:id',
+    element: <div>Detail</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
